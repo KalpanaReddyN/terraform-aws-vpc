4e1c455fa1a6e71b5a724bdfb35aff19316c3395
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-12-11 18:05:50 +0000

    terraform

--- main.tf
+++ main.tf
@@ -67,19 +67,19 @@ resource "aws_subnet" "database" {
   )
 }
 
-# # DB Subnet group for RDS
-# resource "aws_db_subnet_group" "default" {
-#   name       = local.resource_name
-#   subnet_ids = aws_subnet.database[*].id
+# DB Subnet group for RDS
+resource "aws_db_subnet_group" "default" {
+  name       = local.resource_name
+  subnet_ids = aws_subnet.database[*].id
 
-#   tags = merge(
-#     var.common_tags,
-#     var.db_subnet_group_tags,
-#     {
-#         Name = local.resource_name
-#     }
-#   )  
-# }
+  tags = merge(
+    var.common_tags,
+    var.db_subnet_group_tags,
+    {
+        Name = local.resource_name
+    }
+  )  
+}
 
 
 # resource "aws_eip" "nat" {
